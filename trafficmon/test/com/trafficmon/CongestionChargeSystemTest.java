e50f3d46fc7202b2c4e7d3042ea33a73470710f8
fehedwasti <fehedwasi@gmail.com>
2018-12-04 15:12:09 +0000

    Setup testing moduels

---
+++ trafficmon/test/com/trafficmon/CongestionChargeSystemTest.java
@@ -0,0 +1,12 @@
+package com.trafficmon;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CongestionChargeSystemTest {
+
+    @Test
+    public void TestName() {
+    }
+}
