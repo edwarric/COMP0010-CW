047574a6717b87bb44ab4c6cfd44c60da6a52745
Tobias Edwards <tobias_r_edwards@hotmail.co.uk>
2018-12-05 16:47:29 +0000

    Added tests

--- trafficmon/src/com/trafficmon/Example.java
+++ trafficmon/src/com/trafficmon/Example.java
@@ -3,13 +3,14 @@ package com.trafficmon;
 public class Example {
     public static void main(String[] args) throws Exception {
         CongestionChargeSystem congestionChargeSystem = new CongestionChargeSystem();
-        congestionChargeSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"));
+
+        congestionChargeSystem.vehicleEnteringZone(Vehicle.withRegistration("dkjf XYZ"));
         delaySeconds(1);
-        congestionChargeSystem.vehicleEnteringZone(Vehicle.withRegistration("J091 4PY"));
-        delaySeconds(60);
-        congestionChargeSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"));
-        delaySeconds(60);
-        congestionChargeSystem.vehicleLeavingZone(Vehicle.withRegistration("J091 4PY"));
+//        congestionChargeSystem.vehicleEnteringZone(Vehicle.withRegistration("J091 4PY"));
+//        delaySeconds(1);
+        //congestionChargeSystem.vehicleLeavingZone(Vehicle.withRegistration("dkjf XYZ"));
+//        delaySeconds(1);
+//        congestionChargeSystem.vehicleLeavingZone(Vehicle.withRegistration("J041 4PY"));
         congestionChargeSystem.calculateCharges();
     }
     private static void delayMinutes(int mins) throws InterruptedException {
--- trafficmon/test/com/trafficmon/CongestionChargeSystemTest.java
+++ trafficmon/test/com/trafficmon/CongestionChargeSystemTest.java
@@ -6,37 +6,73 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.time.LocalTime;
-import static org.hamcrest.CoreMatchers.containsString;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
 
 public class CongestionChargeSystemTest {
 
     private CongestionChargeSystem CCSystem = new CongestionChargeSystem();
+    private ControllableClock clock = new ControllableClock();
+    OutputStream os = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(os);
 
     @Test
-    public void newEventIsRegisteredInTheLog() {
-        assert CCSystem.getEventlog().isEmpty();
+    public void newEventShouldRegisterInLog() {
+        assertTrue(CCSystem.getEventlog().isEmpty());
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"));
-        assert CCSystem.getEventlog().size() == 1;
+        assertThat(CCSystem.getEventlog().size(), is(1));
     }
-
+    
     @Test
-    public void chargeCalculation(){
-        OutputStream os = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(os);
+    public void exitingUnregisteredVehiclesShouldBeIgnored() {
+        assertTrue(CCSystem.getEventlog().isEmpty());
+        CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A987 XYZ"));
+        assertThat(CCSystem.getEventlog().size(), is(0));
+    }
+    
+    @Test
+    public void unregisteredVehiclesShouldReceivePenaltyNotice() {
         System.setOut(ps);
-        ControllableClock clock = new ControllableClock();
-
+        
+        clock.currentTimeIs(11,00);
+        CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A987 XYZ"), clock);
+        clock.currentTimeIs(12,30);
+        
+        CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A987 XYZ"), clock);
+        CCSystem.calculateCharges();
+        assertThat(os.toString(), containsString("Penalty notice for: Vehicle [A987 XYZ]"));
+    }
+    
+    @Test
+    public void registeredVehiclesChargedCorrectly() throws InterruptedException {
+        System.setOut(ps);
+        
+        CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"));
+        delayMinutes(1);
+        CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"));
+        CCSystem.calculateCharges();
+        
+        assertThat(os.toString(), containsString("£0.05 deducted"));
+    }
+    
+    @Test
+    public void registeredVehiclesChargedCorrectlyUsingSystemClock(){
+        System.setOut(ps);
+        
         clock.currentTimeIs(15,00);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
         clock.currentTimeIs(15,30);
 
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.calculateCharges();
-        //Legacy system should charge £1.50 deducted for 30 minutes
+        
+        //Legacy system should charge £1.50 deducted for 30 minutes (£0.05 per min)
         assertThat(os.toString(), containsString("£1.50 deducted"));
     }
+    
     private class ControllableClock implements Clock{
 
         private LocalTime now;
@@ -49,8 +85,14 @@ public class CongestionChargeSystemTest {
         public void currentTimeIs(int hour, int min){
             now = LocalTime.of(hour, min);
         }
-
-
+        
+    }
+    
+    private static void delayMinutes(int mins) throws InterruptedException {
+        delaySeconds(mins * 60);
+    }
+    private static void delaySeconds(int secs) throws InterruptedException {
+        Thread.sleep(secs * 1000);
     }
 
 }
