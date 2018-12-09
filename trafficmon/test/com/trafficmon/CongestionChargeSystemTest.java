47e2a18fde3041412c6c189790643980a8cf456e
edwarric <tobiasrichardedwards@gmail.com>
2018-12-09 21:25:03 +0000

    Setters for clock and fewer elements in test file

--- trafficmon/test/com/trafficmon/CongestionChargeSystemTest.java
+++ trafficmon/test/com/trafficmon/CongestionChargeSystemTest.java
@@ -16,8 +16,8 @@ public class CongestionChargeSystemTest {
 
     private CongestionChargeSystem CCSystem = new CongestionChargeSystem();
     private ControllableClock clock = new ControllableClock();
-    OutputStream os = new ByteArrayOutputStream();
-    PrintStream ps = new PrintStream(os);
+    private OutputStream os = new ByteArrayOutputStream();
+    private PrintStream ps = new PrintStream(os);
 
     @Test
     public void newEventShouldRegisterInLog() {
@@ -33,13 +33,10 @@ public class CongestionChargeSystemTest {
         assertTrue(CCSystem.getEventlog().isEmpty());
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A987 XYZ"));
         assertThat(CCSystem.getEventlog().size(), is(0));
-        CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A987 XYZ"));
-        assertThat(CCSystem.getEventlog().size(), is(0));
     }
     
     @Test
     public void exitingUnregisteredVehiclesShouldBeIgnoredUsingClock() {
-        clock.currentTimeIs(1, 15, 20);
         assertTrue(CCSystem.getEventlog().isEmpty());
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A987 XYZ"), clock);
         assertThat(CCSystem.getEventlog().size(), is(0));
@@ -48,9 +45,7 @@ public class CongestionChargeSystemTest {
     @Test
     public void unregisteredVehiclesShouldReceivePenaltyNotice() {
         System.setOut(ps);
-        clock.currentTimeIs(1,11, 00);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A987 XYZ"), clock);
-        clock.currentTimeIs(1,12,30);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A987 XYZ"), clock);
         CCSystem.calculateCharges();
         assertThat(os.toString(), containsString("Penalty notice for: Vehicle [A987 XYZ]"));
@@ -59,9 +54,9 @@ public class CongestionChargeSystemTest {
     @Test
     public void registeredVehicleOverstays(){
         System.setOut(ps);
-        clock.currentTimeIs(1,9,00);
+        clock.setHour(9);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
-        clock.currentTimeIs(1, 18, 30);
+        clock.setHour(18);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.calculateCharges();
         assertThat(os.toString(), containsString("£12.00 deducted"));
@@ -70,13 +65,13 @@ public class CongestionChargeSystemTest {
     @Test
     public void registeredVehicleRevisitsAndOverstays(){
         System.setOut(ps);
-        clock.currentTimeIs(1,9,00);
+        clock.setHour(9);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
-        clock.currentTimeIs(1, 9, 30);
+        clock.setHour(10);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
-        clock.currentTimeIs(1,11,00);
+        clock.setHour(11);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
-        clock.currentTimeIs(1, 14, 00);
+        clock.setHour(14);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.calculateCharges();
         assertThat(os.toString(), containsString("£18.00 deducted"));
@@ -85,9 +80,9 @@ public class CongestionChargeSystemTest {
     @Test
     public void registeredVehicleVisitsAndLeavesBefore2(){
         System.setOut(ps);
-        clock.currentTimeIs(1,10,00);
+        clock.setHour(10);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
-        clock.currentTimeIs(1, 13, 00);
+        clock.setHour(13);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.calculateCharges();
         assertThat(os.toString(), containsString("£6.00 deducted"));
@@ -96,9 +91,9 @@ public class CongestionChargeSystemTest {
     @Test
     public void registeredVehicleVisitsAndLeavesAfter2(){
         System.setOut(ps);
-        clock.currentTimeIs(1,15,00);
+        clock.setHour(15);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
-        clock.currentTimeIs(1, 18, 00);
+        clock.setHour(18);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.calculateCharges();
         assertThat(os.toString(), containsString("£4.00 deducted"));
@@ -107,10 +102,10 @@ public class CongestionChargeSystemTest {
     @Test
     public void notEnoughCreditShouldFacePenalty(){
         System.setOut(ps);
-        for (int i=1; i<10; i++) {
-            clock.currentTimeIs(i,00,00);
+        for (int i=1; i<20; i+=2) {
+            clock.setDay(i);
             CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
-            clock.currentTimeIs(i,12,00);
+            clock.setDay(i+1);
             CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
         }
         CCSystem.calculateCharges();
@@ -120,9 +115,9 @@ public class CongestionChargeSystemTest {
     @Test
     public void unorderedVehicleLogShouldTriggerInvestigation(){
         System.setOut(ps);
-        clock.currentTimeIs(1,14,00);
+        clock.setHour(14);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
-        clock.currentTimeIs(1,12,00);
+        clock.setHour(12);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.calculateCharges();
         assertThat(os.toString(), containsString("Mismatched entries/exits. Triggering investigation into " +
@@ -132,7 +127,6 @@ public class CongestionChargeSystemTest {
     @Test
     public void duplicateVehicleEntryShouldTriggerInvestigation(){
         System.setOut(ps);
-        clock.currentTimeIs(1,12,00);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.calculateCharges();
@@ -143,7 +137,6 @@ public class CongestionChargeSystemTest {
     @Test
     public void duplicateVehicleExitShouldTriggerInvestigation(){
         System.setOut(ps);
-        clock.currentTimeIs(1,12,00);
         CCSystem.vehicleEnteringZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
         CCSystem.vehicleLeavingZone(Vehicle.withRegistration("A123 XYZ"), clock);
@@ -153,7 +146,7 @@ public class CongestionChargeSystemTest {
     }
 
     
-    private class ControllableClock implements Clock{
+    static class ControllableClock implements Clock{
 
         private LocalDateTime now;
 
@@ -162,10 +155,33 @@ public class CongestionChargeSystemTest {
             return now;
         }
 
-        public void currentTimeIs(int day, int hour, int min){
-            now = LocalDateTime.of(0,1, day, hour, min,0,0);
+        ControllableClock() {
+            now = LocalDateTime.of(2018, 1, 1, 0, 0, 0);
         }
-        
+
+        void setYear(int year) {
+            now = now.withYear(year);
+        }
+
+        void setMonth(int month) {
+            now = now.withMonth(month);
+        }
+
+        void setDay(int day) {
+            now = now.withDayOfMonth(day);
+        }
+
+        void setHour(int hour) {
+            now = now.withHour(hour);
+        }
+
+        void setMin(int min) {
+            now = now.withMinute(min);
+        }
+
+        void setSec(int sec) {
+            now = now.withSecond(sec);
+        }
     }
     
     private static void delayMinutes(int mins) throws InterruptedException {
@@ -174,5 +190,4 @@ public class CongestionChargeSystemTest {
     private static void delaySeconds(int secs) throws InterruptedException {
         Thread.sleep(secs * 1000);
     }
-
 }
